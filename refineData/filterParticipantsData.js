17820fb4609399f898fefb3c900c74c3fe6b480f
jhamadhav <jhamadhav28@gmail.com>
2022-08-17 18:29:00 +0530

    refining data to extract cc ID

--- refineData/filterParticipantsData.js
+++ refineData/filterParticipantsData.js
@@ -2,22 +2,33 @@ const db = require("./../dbFunc.js")
 let data = db.read("./refineData/rawParticipantsData.json")
 data = JSON.parse(data)
 
-console.log(data);
+// console.log(data);
 
-const monToNum = (inp) => {
-    return new Date(Date.parse(inp + " 1, 2012")).getMonth()
+const getProfileID = (url) => {
+    ls = url.split('/')
+
+    // in case we have / at the end of the url
+    if (url[url.length - 1] == '/') {
+        res = ls[ls.length - 2]
+    } else {
+        res = ls[ls.length - 1]
+    }
+    // console.log(res);
+    return res
 }
+
 
 data = data.map((elem) => {
     return {
         "firstName": elem["First Name"],
         "lastName": elem["Last Name"],
         "name": elem["First Name"] + " " + elem["Last Name"],
+        "profileID": getProfileID(elem["CodeChef Profile Link"]),
         "profileLink": elem["CodeChef Profile Link"],
     }
 })
 
-console.log(data[0]);
+// console.log(data[0]);
 
 let participants = {
     "profiles": data
